c0606c88e4ed0c61310f587943c442c040757807
Caleb Wang <cw@berkeley.edu>
2013-12-03 16:04:35 -0800

    Working position/velocity programs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var gridSize = 256;
 var numParticles = gridSize*gridSize;
-var particlePositions = new Array();
 var debug = false;
 var auto = true;
 
@@ -31,6 +30,8 @@ function random() {
 
 /* initializes randomly distributedparticles */
 function initParticles() {
+  var particlePositions = new Array();
+  var particleVelocities = new Array();
   var numP = numParticles;
   while(numP--) {
     var pX = random() * 2 - 1,
@@ -41,9 +42,16 @@ function initParticles() {
     particlePositions.push(pY);
     particlePositions.push(pZ);
     particlePositions.push(1.0);
+
+    particleVelocities.push(5*(random()*2 - 1));
+    particleVelocities.push(5*(random()*2 - 1));
+    particleVelocities.push(5*(random()*2 - 1));
+    particleVelocities.push(1.0);
   }
 
   particlePositionData = new Float32Array(particlePositions);
+  particleVelocityData = new Float32Array(particleVelocities);
+  console.log(particleVelocities);
 
 }
 
@@ -126,19 +134,21 @@ function initShaders() {
       fs = getShader(gl, "render-fs");
   }
   renderProgram = createProgram(vs, fs);
-
   renderProgram.attributes = []
 
   var physicsvs = getShader(gl, "physics-vs");
   var physicsfs = getShader(gl, "physics-fs");
 
   physicsProgram = createProgram(physicsvs, physicsfs);
-
   physicsProgram.attributes = []
 
+  var velocityvs = getShader(gl, "velocity-vs");
+  var velocityfs = getShader(gl, "velocity-fs");
 
-  //Initialize shader variables
+  velocityProgram = createProgram(velocityvs, velocityfs);
+  velocityProgram.attributes = [];
 
+  //Initialize shader variables and locations
 
   renderProgram.particleIndexAttribute = gl.getAttribLocation(renderProgram, "aParticleIndex");
   renderProgram.attributes.push(renderProgram.particleIndexAttribute);
@@ -151,32 +161,67 @@ function initShaders() {
 
   renderProgram.particlePositionDataLocation = gl.getUniformLocation(renderProgram, "uParticlePositionData");
 
-
+  //position program
   physicsProgram.particlePositionDataLocation = gl.getUniformLocation(physicsProgram, "uParticlePositionData");
+  physicsProgram.particleVelocityDataLocation = gl.getUniformLocation(physicsProgram, "uParticleVelocityData");
   physicsProgram.viewportSizeLocation = gl.getUniformLocation(physicsProgram, "uViewportSize");
   physicsProgram.gridSizeLocation = gl.getUniformLocation(physicsProgram, "uGridSize");
 
-  physicsProgram.particleIndexAttribute = gl.getAttribLocation(renderProgram, "aParticleIndex");
-  physicsProgram.attributes.push(physicsProgram.particleIndexAttribute);
-  console.log(physicsProgram.particleIndexAttribute);
-  gl.enableVertexAttribArray(physicsProgram.particleIndexAttribute);
 
-  physicsProgram.vertexIndexAttribute = gl.getAttribLocation(physicsProgram, "aVertexIndex");
-  console.log(physicsProgram.vertexIndexAttribute);
-  physicsProgram.attributes.push(physicsProgram.vertexIndexAttribute);
-  gl.enableVertexAttribArray(physicsProgram.vertexIndexAttribute);
+  physicsProgram.vertexCoordAttribute = gl.getAttribLocation(physicsProgram, "aVertexCoord");
+  console.log(physicsProgram.vertexCoordAttribute);
+  physicsProgram.attributes.push(physicsProgram.vertexCoordAttribute);
+  gl.enableVertexAttribArray(physicsProgram.vertexCoordAttribute);
   
+  //velocity program
+  velocityProgram.particlePositionDataLocation = gl.getUniformLocation(velocityProgram, "uParticlePositionData");
+  velocityProgram.particleVelocityDataLocation = gl.getUniformLocation(velocityProgram, "uParticleVelocityData");
+  velocityProgram.viewportSizeLocation = gl.getUniformLocation(velocityProgram, "uViewportSize");
+  velocityProgram.gridSizeLocation = gl.getUniformLocation(velocityProgram, "uGridSize");
 
+ 
+  velocityProgram.vertexCoordAttribute = gl.getAttribLocation(velocityProgram, "aVertexCoord");
+  console.log(velocityProgram.vertexCoordAttribute);
+  velocityProgram.attributes.push(velocityProgram.vertexCoordAttribute);
+  gl.enableVertexAttribArray(velocityProgram.vertexCoordAttribute);
+
+  //Create buffers and textures and framebuffers
   // Create particles
+  // Not needed
   particlePositionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, particlePositionBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, particlePositionData, gl.DYNAMIC_DRAW);
   particlePositionBuffer.itemSize = 4;
   particlePositionBuffer.numItems = numParticles;
-  console.log(particlePositions);
 
-  // Create textures and framebuffers and whatnot
+  // Create indices
+  particleIndexData = new Float32Array(numParticles);
+  for (var i = 0, u = 0, v = 1; i < numParticles; i++, u = i * 2, v = u + 1) {
+    particleIndexData[i] = i;
+  }
 
+  particleIndexBuffer = gl.createBuffer();
+  particleIndexBuffer.itemSize = 1;
+  particleIndexBuffer.numItems = numParticles;
+  gl.bindBuffer(gl.ARRAY_BUFFER, particleIndexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, particleIndexData, gl.STATIC_DRAW);
+  gl.enableVertexAttribArray(renderProgram.particleIndexAttribute);
+
+
+ // Create quad vertices
+ var viewportQuadVertices = new Float32Array([
+           -1.0, -1.0,
+           1.0, -1.0,
+           -1.0, 1.0,
+           1.0, 1.0
+  ]);
+
+  viewportQuadBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, viewportQuadBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, viewportQuadVertices, gl.STATIC_DRAW);
+
+
+  //particlePosition
   particlePositionTexture = gl.createTexture();
   particlePositionTexture.unit = 0;
 
@@ -199,54 +244,57 @@ function initShaders() {
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-
   // Create a framebuffer to write data to
-  particleFramebuffer = gl.createFramebuffer();
-  particleFramebuffer.width = gridSize;
-  particleFramebuffer.height = gridSize;
-  gl.bindFramebuffer(gl.FRAMEBUFFER, particleFramebuffer);
+  particlePositionFramebuffer = gl.createFramebuffer();
+  particlePositionFramebuffer.width = gridSize;
+  particlePositionFramebuffer.height = gridSize;
+  gl.bindFramebuffer(gl.FRAMEBUFFER, particlePositionFramebuffer);
   gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, particlePositionTexture, 0);
 
 
+  // particle velocity
+  particleVelocityTexture = gl.createTexture();
+  particleVelocityTexture.unit = 0;
+
+  gl.activeTexture(gl.TEXTURE1 + particleVelocityTexture.unit);
+  gl.bindTexture(gl.TEXTURE_2D, particleVelocityTexture);
+
+  gl.texImage2D(
+    gl.TEXTURE_2D, 0, gl.RGBA, gridSize, gridSize,
+    0, gl.RGBA, gl.FLOAT, particleVelocityData
+  );
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+  particleVelocityFramebuffer = gl.createFramebuffer();
+  particleVelocityFramebuffer.width = gridSize;
+  particleVelocityFramebuffer.height = gridSize;
+  gl.bindFramebuffer(gl.FRAMEBUFFER, particleVelocityFramebuffer);
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, particleVelocityTexture, 0);
+
+  // Set uniforms
   gl.useProgram(renderProgram);
   gl.uniform1i(renderProgram.particlePositionDataLocation, particlePositionTexture.unit);
-  //create coordinates between 0 and 1 for vertex shader to access texture
-  var interval = 1.0/gridSize;
-
-  particleIndexData = new Float32Array(numParticles);
-  for (var i = 0, u = 0, v = 1; i < numParticles; i++, u = i * 2, v = u + 1){
-    particleIndexData[i] = i;
-  }
-
-  particleIndexBuffer = gl.createBuffer();
-  particleIndexBuffer.itemSize = 1;
-  particleIndexBuffer.numItems = numParticles;
-  gl.bindBuffer(gl.ARRAY_BUFFER, particleIndexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, particleIndexData, gl.STATIC_DRAW);
-  gl.enableVertexAttribArray(renderProgram.particleIndexAttribute);
-
-
- var viewportQuadVertices = new Float32Array([
-           -1.0, -1.0,
-           1.0, -1.0,
-           -1.0, 1.0,
-           1.0, 1.0
-  ]);
-
-  viewportQuadBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, viewportQuadBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, viewportQuadVertices, gl.STATIC_DRAW);
-
-
   gl.useProgram(renderProgram);
 
   gl.uniform1f(renderProgram.gridSizeLocation, gridSize);
-
   gl.useProgram(physicsProgram);
 
   gl.uniform2f(physicsProgram.viewportSizeLocation, gridSize, gridSize);
   gl.uniform1i(physicsProgram.particlePositionDataLocation, 0);
+  //What is this instead of 0?
+  gl.uniform1i(physicsProgram.particleVelocityDataLocation, 1);
   gl.uniform1f(physicsProgram.gridSizeLocation, gridSize);
+
+  gl.useProgram(velocityProgram);
+  gl.uniform2f(velocityProgram.viewportSizeLocation, gridSize, gridSize);
+  gl.uniform1i(velocityProgram.particlePositionDataLocation, 0);
+  //What is this instead of 0?
+  gl.uniform1i(velocityProgram.particleVelocityDataLocation, 1);
+  gl.uniform1f(velocityProgram.gridSizeLocation, gridSize);
 }
 
 /* set perspective and translation matricies so shader can read */
@@ -260,39 +308,49 @@ function render() {
   if (auto) {
       requestAnimFrame(render);
   }
-  updateScene();
+  updateVelocities();
+  updatePositions();
   drawScene();
 }
 
-function updateScene() {
+function updateVelocities() {
+  console.log("updating velocities");
+  enableAttributes(velocityProgram);
+  gl.useProgram(velocityProgram);
+
+  gl.viewport(0, 0, gridSize, gridSize);
+  gl.activeTexture(gl.TEXTURE1);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, viewportQuadBuffer);
+  gl.vertexAttribPointer(velocityProgram.vertexCoordAttribute, 2, gl.FLOAT, gl.FALSE, 0, 0);
+
+  gl.bindFramebuffer(gl.FRAMEBUFFER, particleVelocityFramebuffer);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
+function updatePositions() {
   console.log('updating scene');
 
   gl.useProgram(physicsProgram);
-  disableAttributes(renderProgram);
+  //disableAttributes(renderProgram);
   enableAttributes(physicsProgram);
   gl.viewport(0, 0, gridSize, gridSize);
 
-  //gl.bindBuffer(gl.ARRAY_BUFFER, viewportQuadBuffer);
-  //gl.vertexAttribPointer(physicsProgram.vertexPositionAttribute, 2, gl.FLOAT, gl.FALSE, 0, 0);
-
   gl.activeTexture(gl.TEXTURE0);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, particleIndexBuffer);
-  gl.vertexAttribPointer(physicsProgram.particleIndexAttribute, 1, gl.FLOAT, gl.FALSE, 0, 0);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, viewportQuadBuffer);
-  gl.vertexAttribPointer(physicsProgram.vertexIndexAttribute, 2, gl.FLOAT, gl.FALSE, 0, 0);
+  gl.vertexAttribPointer(physicsProgram.vertexCoordAttribute, 2, gl.FLOAT, gl.FALSE, 0, 0);
 
-  gl.bindFramebuffer(gl.FRAMEBUFFER, particleFramebuffer);
+  gl.bindFramebuffer(gl.FRAMEBUFFER, particlePositionFramebuffer);
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-  //gl.drawArrays(gl.POINTS, 0, numParticles);
 }
 
 function drawScene() {
   console.log('rendering scene');
   gl.useProgram(renderProgram);
   enableAttributes(renderProgram);
-  disableAttributes(physicsProgram);
+  //disableAttributes(physicsProgram);
 
   gl.bindFramebuffer(gl.FRAMEBUFFER, null);
   gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
@@ -300,7 +358,7 @@ function drawScene() {
 
   mat4.perspective(pMatrix, .78539, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
   mat4.identity(mvMatrix);
-  mat4.translate(mvMatrix, mvMatrix,[0.0, 0.0, -2.0]);
+  mat4.translate(mvMatrix, mvMatrix,[0.0, 0.0, -5.0]);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, particleIndexBuffer);
   gl.enableVertexAttribArray(renderProgram.particleIndexAttribute);
